1d2b29110faf14cc6895ba61f27ceab879afc74d
Samyak Jain <34805671+samyakjain1234@users.noreply.github.com>
2022-10-06 12:41:21 +0530

    Create Sales by Match.py

---
+++ HackerRank/OneMonthPrep/Sales by Match.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    count = 0
+    freq = {}
+    for items in ar:
+        freq[items] = ar.count(items)
+    for key,value in freq.items():
+        count += value//2
+    return count
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
