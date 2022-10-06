8492e72bb344a2ba492bfcf0c0ae4c33e38ab47e
Samyak Jain <34805671+samyakjain1234@users.noreply.github.com>
2022-10-06 12:47:09 +0530

    Create Max Min.py

---
+++ HackerRank/OneMonthPrep/Max Min.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxMin' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def maxMin(k, arr):
+    # Write your code here
+    arr = sorted(arr)
+    i=0
+    j=k-1
+    m = arr[-1]
+    while(j<len(arr)):
+        res = arr[j] - arr[i]
+        m = min(m,res)
+        i = i+1
+        j = j+1
+    return m
+    
+       
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    k = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
