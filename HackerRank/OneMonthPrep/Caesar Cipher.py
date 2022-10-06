964e702fb17b5031517100041c2e2e5125665004
Samyak Jain <34805671+samyakjain1234@users.noreply.github.com>
2022-10-06 12:45:55 +0530

    Create Caesar Cipher.py

---
+++ HackerRank/OneMonthPrep/Caesar Cipher.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'caesarCipher' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. INTEGER k
+#
+
+def caesarCipher(s, k):
+    # Write your code here
+    result = ''
+    for char in s:
+            if char.isupper() and char.isalpha():
+                result += chr((ord(char)+k-65)%26 +65)
+            elif char.islower() and char.isalpha():
+                result += chr((ord(char)+k-97)%26 + 97)
+            else:
+                result += char        
+    return result
+                
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = input()
+
+    k = int(input().strip())
+
+    result = caesarCipher(s, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
