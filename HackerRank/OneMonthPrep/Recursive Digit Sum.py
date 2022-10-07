7d2349fe46ff8e2260a5150c4af13a7b5b529b9d
Samyak Jain <34805671+samyakjain1234@users.noreply.github.com>
2022-10-07 23:33:28 +0530

    Create Recursive Digit Sum.py

---
+++ HackerRank/OneMonthPrep/Recursive Digit Sum.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING n
+#  2. INTEGER k
+#
+
+def superDigit(n, k):
+    newNum = sum(list(map(int,n)))
+    recSum = newNum * k
+    while(recSum > 9):
+        totSum = sum(list(map(int,str(recSum))))
+        recSum = totSum
+    return recSum
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = first_multiple_input[0]
+
+    k = int(first_multiple_input[1])
+
+    result = superDigit(n, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
